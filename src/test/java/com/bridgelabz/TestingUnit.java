f429d8e81c37905b0b7153199f9f89f3c835d9b5
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-07-03 12:59:23 +0530

    addd multiple entry sample emails

--- src/test/java/com/bridgelabz/TestingUnit.java
+++ src/test/java/com/bridgelabz/TestingUnit.java
@@ -8,6 +8,13 @@ public class TestingUnit {
 
     UserRegistration userReg = new UserRegistration();
 
+    public String[] validemail = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+            "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com" };
+
+    public String[] unvalidemail = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
+            "..abc@abc.com", ".abc()@gmail.com", "abc@%.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a",
+            "abc@gmail.com.aa.au" };
+
 
     @Test
     public void firstName() {
@@ -38,6 +45,31 @@ public class TestingUnit {
         boolean result = userReg.validateEmail("yahhoo@123.com");
         assertEquals(true, result);
     }
+
+    @Test
+    public void validEmailTest() {
+        for (String sample : validemail) {
+
+            String checker1 = sample;
+
+            Boolean email = userReg.validateEmail(checker1);
+            assertEquals(true, email);
+        }
+
+    }
+
+
+    @Test
+    public void unvalidEmailTest() {
+
+        for (String sample : unvalidemail) {
+            String checker1 = sample;
+
+            Boolean email = userReg.validateEmail(checker1);
+            assertEquals(false, email);
+        }
+    }
+
 
 
 
