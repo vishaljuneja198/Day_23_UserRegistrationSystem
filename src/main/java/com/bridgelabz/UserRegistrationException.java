9c84397be87c8bcabed1e316a3ec372b1e148dc8
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-07-03 13:25:47 +0530

    added custom exception for invaild problem

---
+++ src/main/java/com/bridgelabz/UserRegistrationException.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+
+public class UserRegistrationException extends Exception {
+
+    public UserRegistrationException(String message) {
+        super(message);
+    }
+}
--- src/test/java/com/bridgelabz/TestingUnit.java
+++ src/test/java/com/bridgelabz/TestingUnit.java
@@ -1,77 +1,77 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class TestingUnit {
-
-    UserRegistration userReg = new UserRegistration();
-
-    public String[] validemail = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
-            "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com" };
-
-    public String[] unvalidemail = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
-            "..abc@abc.com", ".abc()@gmail.com", "abc@%.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a",
-            "abc@gmail.com.aa.au" };
 
 
     @Test
-    public void firstName() {
-        boolean result = userReg.validateFirstName("vishal");
-        assertEquals(true, result);
+    public void firstNames() {
+
+        UserRegistration userReg = new UserRegistration("vishal");
+
+        try {
+            boolean result = userReg.validateFirstName();
+        } catch (UserRegistrationException e) {
+
+            System.out.println(e);
+        }
+
     }
+
 
     @Test
     public void lastName() {
-        boolean result = userReg.validateLastName("juneja ");
-        assertEquals(true, result);
+        UserRegistration userReg = new UserRegistration("juneja");
+        try {
+            boolean result = userReg.validateLastName();
+        } catch (UserRegistrationException e) {
+
+            // e.printStackTrace();
+
+            System.out.println(e);
+        }
     }
+
 
     @Test
     public void phoneNumber() {
-        boolean result = userReg.validatePhoneNumber("1234567890");
-        assertEquals(true, result);
+        UserRegistration userReg = new UserRegistration("1234567890");
+        try {
+            boolean result = userReg.validatePhoneNumber();
+        } catch (UserRegistrationException e) {
+
+            // e.printStackTrace();
+
+            System.out.println(e);
+        }
     }
+
 
     @Test
     public void password() {
-        boolean result = userReg.validatePassword("xxxxxxxx");
-        assertEquals(true, result);
+        UserRegistration userReg = new UserRegistration("Vish@123");
+        try {
+            boolean result = userReg.validatePassword();
+        } catch (UserRegistrationException e) {
+
+            // e.printStackTrace();
+
+            System.out.println(e);
+        }
     }
+
 
     @Test
     public void email() {
-        boolean result = userReg.validateEmail("yahhoo@123.com");
-        assertEquals(true, result);
-    }
+        UserRegistration userReg = new UserRegistration("vishaljuneja198l@gmail.com");
+        try {
+            boolean result = userReg.validateEmail();
+        } catch (UserRegistrationException e) {
 
-    @Test
-    public void validEmailTest() {
-        for (String sample : validemail) {
+            // e.printStackTrace();
 
-            String checker1 = sample;
-
-            Boolean email = userReg.validateEmail(checker1);
-            assertEquals(true, email);
-        }
-
-    }
-
-
-    @Test
-    public void unvalidEmailTest() {
-
-        for (String sample : unvalidemail) {
-            String checker1 = sample;
-
-            Boolean email = userReg.validateEmail(checker1);
-            assertEquals(false, email);
+            System.out.println(e);
         }
     }
-
-
-
 
 }
-
